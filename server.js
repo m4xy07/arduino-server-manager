d3da7fdd107fcbe648079132dbcb8e1e374ac6c9
Maximus <aman.10shaikh@gmail.com>
2024-04-09 13:46:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/data', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
